58e0d382922a055ad1c2e50448749eb44a9a25e6
varpu19 <vap8665@rit.edu>
2021-03-18 01:01:36 -0400

    led grid

--- scripts/LED.js
+++ scripts/LED.js
@@ -2,9 +2,14 @@ class LED {
     constructor(xpos, ypos) {
         this.xpos = xpos;
         this.ypos = ypos;
-        this.color = (255, 255, 255)
+        this.color = {
+            r: 133,
+            g: 133,
+            b: 133
+        }
     }
     draw() {
-        ellipse(this.xpos, this.ypos)
+        fill(this.color.r, this.color.g, this.color.b)
+        ellipse(this.xpos, this.ypos, 25)
     }
 }
--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,18 +1,22 @@
 let capCanvas = document.querySelector("#cap");
 // var ctx = canvas.getContext('2d');
 // let cols = [];
-
+let color = { r: 255, g: 255, b: 255 };
 let leds = [];
 let cSize = window.innerWidth
+let last = [];
+let current = [];
+let next = [];
 
 function setup() {
-    let canvas = createCanvas(cSize, cSize)
+    let canvas = createCanvas(cSize, cSize + 300)
+    canvas.id = "capCanvas"
     canvas.parent(capCanvas);
     for (let col = 0; col < 32; col++) {
         let rows = [];
         for (let row = 0; row < 32; row++) {
 
-            let led = new LED(col, row);
+            let led = new LED((col * 29.5) + 35, (row * 29.5) + 185);
             rows.push(led);
         }
         leds.push(rows)
@@ -24,9 +28,29 @@ function draw() {
     // cSize = window.innerWidth;
     background(23, 35, 67)
     fill(0, 52, 153)
-    rect(20, 20, cSize - 40, cSize - 40, 5)
+    rect(20, 175, cSize - 40, cSize - 40, 5)
+
+    for (let c in leds) {
+        for (let l in leds[c]) {
+            if (mouseX > leds[c][l].x - 12 &&
+                mouseX < leds[c][l].x + 12 &&
+                mouseY > leds[c][l].y - 12 &&
+                mouseX < leds[c][l].y + 12) {
+                leds[c][l].color = color
+                console.log("mouseX")
+            }
+            // if (mouseIsPressed) {
+
+
+            // }
+            leds[c][l].draw()
+        }
+    }
 }
 
+function mousePressed() {
+
+}
 window.onresize = windowResized;
 
 function windowResized() {
